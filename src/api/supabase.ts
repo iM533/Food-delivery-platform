80a90d8f341bbe8969c291836396d0c59ba0d2cb
Aleksei <5f4ff4ff@gmail.com>
2026-03-03 18:33:26 +0200

    Extract typed supabase client

---
+++ src/api/supabase.ts
@@ -0,0 +1,5 @@
+
+import {createClient} from "@supabase/supabase-js";
+import type {Database} from "../types/database.types.ts";
+
+export const supabase = createClient<Database>(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY)
